0e70bcaec0841aee90ae42b27b86ec985576b268
priolilucas1 <priolilucas1@gmail.com>
2021-06-09 16:06:58 -0300

    Base Screen Started

---
+++ lib/src/screens/base/base_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:loja_virtual/src/common/custom_drawer/custom_drawer.dart';
+import 'package:loja_virtual/src/models/page_manager.dart';
+import 'package:provider/provider.dart';
+
+class BaseScreen extends StatelessWidget {
+  final PageController pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: Text('Home'),
+            ),
+          ),
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+        ],
+      ),
+    );
+  }
+}
