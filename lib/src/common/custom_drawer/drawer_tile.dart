2c8aef1af731825ebc6fd00980a2649c6c2e0800
priolilucas1 <priolilucas1@gmail.com>
2021-06-09 16:06:47 -0300

    Provider Connected With Drawer Tile

--- lib/src/common/custom_drawer/drawer_tile.dart
+++ lib/src/common/custom_drawer/drawer_tile.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/src/models/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class DrawerTile extends StatelessWidget {
-  DrawerTile({required this.iconData, required this.title});
+  DrawerTile({required this.iconData, required this.title, required this.page});
 
   final IconData iconData;
   final String title;
+  final int page;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(
-          iconData,
-          size: 32,
-          color: Colors.grey[700],
-        ),
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: 16,
-            color: Colors.grey[700],
+    final int curPage = context.watch<PageManager>().page;
+
+    return InkWell(
+      onTap: () {
+        context.read<PageManager>().setPage(page);
+      },
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 12),
+            child: Icon(
+              iconData,
+              size: 32,
+              color: curPage == page ? Colors.red : Colors.grey[700],
+            ),
           ),
-        ),
-      ],
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16,
+              color: curPage == page ? Colors.red : Colors.grey[700],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
