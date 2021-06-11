e6f53b8ce9a2cc825baa73d51ea2c7a20bfb415f
priolilucas1 <priolilucas1@gmail.com>
2021-06-11 13:46:23 -0300

    Fail and Success - Created

--- lib/src/models/user_manager.dart
+++ lib/src/models/user_manager.dart
@@ -1,18 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:loja_virtual/src/helpers/firebase_errors.dart';
 import 'package:loja_virtual/src/models/user.dart' as us;
 
 class UserManager {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
-  Future<void> signIn(us.User user) async {
+  Future<void> signIn({
+    required us.User user,
+    required Function onFail,
+    required Function onSuccess,
+  }) async {
     try {
       final UserCredential result = await auth.signInWithEmailAndPassword(
         email: user.email,
         password: user.password,
       );
-      print(result.user?.uid);
+
+      onSuccess();
     } on FirebaseAuthException catch (e) {
-      print(e);
+      onFail(getErrorString(e.code));
     }
   }
 }
