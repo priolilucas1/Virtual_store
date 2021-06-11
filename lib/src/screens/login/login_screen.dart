4762a946edbaeb3a359ebe56774ea5f506e118d5
priolilucas1 <priolilucas1@gmail.com>
2021-06-11 14:28:49 -0300

    Consumer with User Manager Added

--- lib/src/screens/login/login_screen.dart
+++ lib/src/screens/login/login_screen.dart
@@ -23,84 +23,99 @@ class LoginScreen extends StatelessWidget {
           margin: const EdgeInsets.symmetric(horizontal: 18),
           child: Form(
             key: formKey,
-            child: ListView(
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              children: <Widget>[
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(hintText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  validator: (email) {
-                    if (!emailValid(email ?? 'null@null.com')) {
-                      return 'Email inválido';
-                    }
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  controller: passwordController,
-                  decoration: const InputDecoration(hintText: 'Senha'),
-                  autocorrect: false,
-                  obscureText: true,
-                  validator: (password) {
-                    if (password == null || password.length < 6) {
-                      return 'Senha inválida';
-                    }
-                    return null;
-                  },
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
+            child: Consumer<UserManager>(
+              builder: (_, userManager, __) {
+                return ListView(
+                  padding: const EdgeInsets.all(16),
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: emailController,
+                      enabled: !userManager.loading,
+                      decoration: const InputDecoration(hintText: 'E-mail'),
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      validator: (email) {
+                        if (!emailValid(email ?? 'null@null.com')) {
+                          return 'Email inválido';
+                        }
+                      },
                     ),
-                    child: const Text('Esqueci minha senha'),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                SizedBox(
-                  height: 44,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (formKey.currentState?.validate() ?? false) {
-                        context.read<UserManager>().signIn(
-                              user: User(
-                                emailController.text,
-                                passwordController.text,
-                              ),
-                              onFail: (e) {
-                                ScaffoldMessenger.of(context)
-                                    .showSnackBar(SnackBar(
-                                  content: Text(
-                                    e as String,
-                                    style: TextStyle(fontSize: 18),
-                                  ),
-                                  backgroundColor: Colors.red,
-                                ));
-                              },
-                              onSuccess: () {
-                                // TODO: FECHAR TELA DE LOGIN
-                              },
-                            );
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Theme.of(context).primaryColor,
-                      onPrimary: Colors.white,
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: passwordController,
+                      enabled: !userManager.loading,
+                      decoration: const InputDecoration(hintText: 'Senha'),
+                      autocorrect: false,
+                      obscureText: true,
+                      validator: (password) {
+                        if (password == null || password.length < 6) {
+                          return 'Senha inválida';
+                        }
+                        return null;
+                      },
                     ),
-                    child: const Text(
-                      'Entrar',
-                      style: TextStyle(
-                        fontSize: 18,
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                        ),
+                        child: const Text('Esqueci minha senha'),
                       ),
                     ),
-                  ),
-                ),
-              ],
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      height: 44,
+                      child: ElevatedButton(
+                        onPressed: userManager.loading
+                            ? null
+                            : () {
+                                if (formKey.currentState?.validate() ?? false) {
+                                  userManager.signIn(
+                                    user: User(
+                                      emailController.text,
+                                      passwordController.text,
+                                    ),
+                                    onFail: (e) {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                        content: Text(
+                                          e as String,
+                                          style: const TextStyle(fontSize: 18),
+                                        ),
+                                        backgroundColor: Colors.red,
+                                      ));
+                                    },
+                                    onSuccess: () {
+                                      // TODO: FECHAR TELA DE LOGIN
+                                    },
+                                  );
+                                }
+                              },
+                        style: ElevatedButton.styleFrom(
+                          primary: Theme.of(context).primaryColor,
+                          onPrimary: Colors.white,
+                          onSurface:
+                              Theme.of(context).primaryColor.withAlpha(100),
+                        ),
+                        child: userManager.loading
+                            ? const CircularProgressIndicator(
+                                valueColor:
+                                    AlwaysStoppedAnimation(Colors.white),
+                              )
+                            : const Text(
+                                'Entrar',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                ),
+                              ),
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
         ),
