2d368d5931d7047bf115e046587f424938c5eb23
priolilucas1 <priolilucas1@gmail.com>
2021-06-11 14:04:29 -0300

    Change Notifier Provider - Added

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider(
+    return ChangeNotifierProvider(
       create: (_) => UserManager(),
       child: MaterialApp(
         title: 'Loja Prioli',
