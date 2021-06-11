3f3e2a08aa18f2dd1653327c461ca2839742b6a1
priolilucas1 <priolilucas1@gmail.com>
2021-06-11 14:29:48 -0300

    Loading is turned private

--- lib/src/models/user_manager.dart
+++ lib/src/models/user_manager.dart
@@ -1,15 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:loja_virtual/src/helpers/firebase_errors.dart';
 import 'package:loja_virtual/src/models/user.dart' as us;
 
-class UserManager {
+class UserManager extends ChangeNotifier {
   final FirebaseAuth auth = FirebaseAuth.instance;
+
+  bool _loading = false;
+  bool get loading => _loading;
 
   Future<void> signIn({
     required us.User user,
     required Function onFail,
     required Function onSuccess,
   }) async {
+    loading = true;
     try {
       final UserCredential result = await auth.signInWithEmailAndPassword(
         email: user.email,
@@ -20,5 +25,12 @@ class UserManager {
     } on FirebaseAuthException catch (e) {
       onFail(getErrorString(e.code));
     }
+
+    loading = false;
+  }
+
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
   }
 }
