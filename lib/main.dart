6f9d17efcf8046d0ae57b17ba1dda1934be1070e
priolilucas1 <priolilucas1@gmail.com>
2021-06-14 16:39:12 -0300

    Login Route Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:loja_virtual/src/screens/signup/signup_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:loja_virtual/src/models/user_manager.dart';
 import 'package:loja_virtual/src/screens/base/base_screen.dart';
+
+import 'package:loja_virtual/src/screens/login/login_screen.dart';
+import 'package:loja_virtual/src/screens/signup/signup_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,9 +34,14 @@ class MyApp extends StatelessWidget {
         initialRoute: '/base',
         onGenerateRoute: (settings) {
           switch (settings.name) {
+            case '/login':
+              return MaterialPageRoute(
+                builder: (_) => LoginScreen(),
+              );
             case '/signup':
-              return MaterialPageRoute(builder: (_) => SignUpScreen());
-
+              return MaterialPageRoute(
+                builder: (_) => SignUpScreen(),
+              );
             case '/base':
             default:
               return MaterialPageRoute(builder: (_) => BaseScreen());
