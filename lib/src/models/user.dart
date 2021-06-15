f3602236eeb564272104c74bc7f5973b75191a0d
priolilucas1 <priolilucas1@gmail.com>
2021-06-15 10:14:44 -0300

    User Data Connection Done

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -1,15 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
-  User([
-    this.id,
-    this.email,
-    this.name,
-    this.password,
-    this.confirmPassword,
-  ]);
+  User({this.id, this.email, this.name, this.password, this.confirmPassword});
 
   String? id;
   String? email;
-  String? password;
   String? name;
+  String? password;
   String? confirmPassword;
+
+  User.fromDocument(DocumentSnapshot document) {
+    if (document.exists) {
+      id = document.id;
+      name = document['name'] as String;
+      email = document['email'] as String;
+    }
+  }
+
+  DocumentReference get firestoreRef =>
+      FirebaseFirestore.instance.doc('users/$id');
+
+  Future<void> saveData() async {
+    await firestoreRef.set(toMap());
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+    };
+  }
 }
