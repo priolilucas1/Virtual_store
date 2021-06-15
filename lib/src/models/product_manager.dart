8cd1e60454f2b28caf46affe31de067670c8e6d1
priolilucas1 <priolilucas1@gmail.com>
2021-06-15 15:59:58 -0300

    Search Filter Done

--- lib/src/models/product_manager.dart
+++ lib/src/models/product_manager.dart
@@ -10,13 +10,38 @@ class ProductManager extends ChangeNotifier {
 
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  List<Product> _allProducts = [];
+  List<Product> allProducts = [];
+
+  String _search = '';
+
+  String get search => _search;
+  set search(String value) {
+    _search = value;
+    notifyListeners();
+  }
+
+  List<Product> get filteredProducts {
+    final List<Product> filteredProducts = [];
+
+    if (search.isEmpty) {
+      filteredProducts.addAll(allProducts);
+    } else {
+      filteredProducts.addAll(
+        allProducts.where(
+          (product) =>
+              product.name!.toLowerCase().contains(search.toLowerCase()),
+        ),
+      );
+    }
+
+    return filteredProducts;
+  }
 
   Future<void> _loadAllProducts() async {
     final QuerySnapshot snapProducts =
         await firestore.collection('products').get();
 
-    _allProducts =
+    allProducts =
         snapProducts.docs.map((doc) => Product.fromDocument(doc)).toList();
 
     notifyListeners();
