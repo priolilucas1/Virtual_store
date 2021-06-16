9303497083b806ba1e830c850b5cf41c01569d90
priolilucas1 <priolilucas1@gmail.com>
2021-06-16 14:50:12 -0300

    Product Screen UI Created

---
+++ lib/src/screens/product/product_screen.dart
@@ -0,0 +1,88 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/src/models/product.dart';
+
+class ProductScreen extends StatelessWidget {
+  const ProductScreen(this.product);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.name!),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 400,
+              enableInfiniteScroll: false,
+              enlargeCenterPage: true,
+              aspectRatio: 1,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 15),
+            ),
+            items: product.images?.map((url) {
+              return Builder(builder: (BuildContext context) {
+                return Image(
+                  image: NetworkImage(url),
+                );
+              });
+            }).toList(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  product.name!,
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    'A partir de',
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+                Text(
+                  'R\$ 19.99',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 8),
+                  child: Text(
+                    'Descrição',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Text(
+                  product.description!,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
