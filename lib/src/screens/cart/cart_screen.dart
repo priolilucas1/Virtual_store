7a07b53687ca8e300f74e58745ed3ab377f766bc
priolilucas1 <priolilucas1@gmail.com>
2021-06-17 16:13:34 -0300

    Cart Screen Created

---
+++ lib/src/screens/cart/cart_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:loja_virtual/src/models/cart_manager.dart';
+
+import 'components/cart_tile.dart';
+
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho'),
+        centerTitle: true,
+      ),
+      body: Consumer<CartManager>(builder: (_, cartManager, __) {
+        return Column(
+          children: cartManager.items
+              .map((cartProduct) => CartTile(cartProduct))
+              .toList(),
+        );
+      }),
+    );
+  }
+}
