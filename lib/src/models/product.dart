f218e06277636548b0900f07a9f6f15edb606bab
priolilucas1 <priolilucas1@gmail.com>
2021-06-17 11:43:26 -0300

    Try Catch Added

--- lib/src/models/product.dart
+++ lib/src/models/product.dart
@@ -1,10 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja_virtual/src/helpers/firebase_errors.dart';
 import 'package:loja_virtual/src/models/item_size.dart';
 
 class Product {
   Product.fromDocument(DocumentSnapshot document) {
-    if (document.exists) {
-      try {
+    try {
+      if (document.exists) {
         id = document.id;
         name = document['name'] as String;
         description = document['description'] as String;
@@ -12,7 +13,9 @@ class Product {
         sizes = (document['sizes'] as List<dynamic>)
             .map((size) => ItemSize.fromMap(size as Map<String, dynamic>))
             .toList();
-      } catch (e) {}
+      }
+    } catch (e) {
+      getErrorString(e.toString());
     }
   }
 
