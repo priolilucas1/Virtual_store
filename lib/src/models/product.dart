9f0cf8549bfaf0b157c6a6e6dc43ffd306167489
priolilucas1 <priolilucas1@gmail.com>
2021-06-17 10:27:05 -0300

    Product Exception Handled

--- lib/src/models/product.dart
+++ lib/src/models/product.dart
@@ -1,17 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja_virtual/src/models/item_size.dart';
 
 class Product {
   Product.fromDocument(DocumentSnapshot document) {
     if (document.exists) {
-      id = document.id;
-      name = document['name'] as String;
-      description = document['description'] as String;
-      images = List<String>.from(document['images'] as List<dynamic>);
+      try {
+        id = document.id;
+        name = document['name'] as String;
+        description = document['description'] as String;
+        images = List<String>.from(document['images'] as List<dynamic>);
+        sizes = (document['sizes'] as List<dynamic>)
+            .map((size) => ItemSize.fromMap(size as Map<String, dynamic>))
+            .toList();
+      } catch (e) {}
     }
   }
 
-  String? id;
-  String? name;
-  String? description;
-  List<String>? images;
+  late String id;
+  late String name;
+  late String description;
+  late List<String> images;
+  List<ItemSize> sizes = [];
 }
