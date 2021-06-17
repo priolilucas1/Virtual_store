0a59c84a212bfb889988c3e3a6a3c9eca3e90f4d
priolilucas1 <priolilucas1@gmail.com>
2021-06-17 13:41:07 -0300

    Size selection implemented

--- lib/src/screens/product/components/size_widget.dart
+++ lib/src/screens/product/components/size_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:loja_virtual/src/models/item_size.dart';
+import 'package:loja_virtual/src/models/product.dart';
 
 class SizeWidget extends StatelessWidget {
   const SizeWidget(this.size);
@@ -8,25 +11,54 @@ class SizeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: !size.hasStock ? Colors.red.withAlpha(50) : Colors.grey,
+    final product = context.watch<Product>();
+    final selected = size == product.selectedSize;
+
+    Color color;
+    if (!size.hasStock) {
+      color = Colors.red.withAlpha(50);
+    } else if (selected) {
+      color = Theme.of(context).primaryColor;
+    } else {
+      color = Colors.grey;
+    }
+
+    return GestureDetector(
+      onTap: () {
+        if (size.hasStock) {
+          product.selectedSize = size;
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: color,
+          ),
         ),
-      ),
-      child: Row(
-        children: [
-          Container(
-            color: !size.hasStock ? Colors.red.withAlpha(50) : Colors.grey,
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: Text(
-              size.name,
-              style: const TextStyle(
-                color: Colors.white,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              color: color,
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+              child: Text(
+                size.name,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-        ],
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Text(
+                'R\$ ${size.price.toStringAsFixed(2)}',
+                style: TextStyle(
+                  color: color,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
