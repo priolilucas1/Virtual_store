25d7df9db3a5391b500854b2b7ba7ba7bcb3484c
priolilucas1 <priolilucas1@gmail.com>
2021-06-21 16:40:25 -0300

    Initializer List Added

--- lib/src/models/product.dart
+++ lib/src/models/product.dart
@@ -1,30 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:loja_virtual/src/helpers/firebase_errors.dart';
 import 'package:loja_virtual/src/models/item_size.dart';
 
 class Product extends ChangeNotifier {
-  Product.fromDocument(DocumentSnapshot document) {
-    try {
-      if (document.exists) {
-        id = document.id;
-        name = document['name'] as String;
-        description = document['description'] as String;
-        images = List<String>.from(document['images'] as List<dynamic>);
-        sizes = (document['sizes'] as List<dynamic>)
+  Product.fromDocument(DocumentSnapshot document)
+      : id = document.id,
+        name = document['name'] as String,
+        description = document['description'] as String,
+        images = List<String>.from(document['images'] as List<dynamic>),
+        sizes = ((document['sizes'] ?? []) as List<dynamic>)
             .map((size) => ItemSize.fromMap(size as Map<String, dynamic>))
             .toList();
-      }
-    } catch (e) {
-      getErrorString(e.toString());
-    }
-  }
 
-  late String id;
-  late String name;
-  late String description;
-  late List<String> images;
-  List<ItemSize> sizes = [];
+  final String id;
+  final String name;
+  final String description;
+  final List<String> images;
+  final List<ItemSize> sizes;
 
   ItemSize? _selectedSize;
 
