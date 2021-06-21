a1794d9d77fc5d461caf0cf98da27b6269e67446
priolilucas1 <priolilucas1@gmail.com>
2021-06-21 12:21:47 -0300

    Home Screen Added

--- lib/src/screens/base/base_screen.dart
+++ lib/src/screens/base/base_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-
-import 'package:loja_virtual/src/common/custom_drawer/custom_drawer.dart';
-import 'package:loja_virtual/src/models/page_manager.dart';
-import 'package:loja_virtual/src/screens/products/products_screen.dart';
 import 'package:provider/provider.dart';
+
+import 'package:loja_virtual/src/models/page_manager.dart';
+
+import 'package:loja_virtual/src/screens/home/home_screen.dart';
+import 'package:loja_virtual/src/screens/products/products_screen.dart';
+import 'package:loja_virtual/src/common/custom_drawer/custom_drawer.dart';
 
 class BaseScreen extends StatelessWidget {
   final PageController pageController = PageController();
@@ -16,13 +18,7 @@ class BaseScreen extends StatelessWidget {
         controller: pageController,
         physics: const NeverScrollableScrollPhysics(),
         children: [
-          Scaffold(
-            drawer: CustomDrawer(),
-            appBar: AppBar(
-              title: const Text('Home'),
-              centerTitle: true,
-            ),
-          ),
+          HomeScreen(),
           ProductsScreen(),
           Scaffold(
             drawer: CustomDrawer(),
