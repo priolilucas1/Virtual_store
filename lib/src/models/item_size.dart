7e732dba59daebc6c8060cfde4367b837a8de01b
priolilucas1 <priolilucas1@gmail.com>
2021-06-21 16:40:10 -0300

    Initializer List Added

--- lib/src/models/item_size.dart
+++ lib/src/models/item_size.dart
@@ -1,18 +1,12 @@
 class ItemSize {
-  ItemSize.fromMap(Map<String, dynamic> map) {
-    name = map['name'] as String;
-    price = map['price'] as num;
-    stock = map['stock'] as int;
-  }
+  ItemSize.fromMap(Map<String, dynamic> map)
+      : name = map['name'] as String,
+        price = map['price'] as num,
+        stock = map['stock'] as int;
 
-  late String name;
-  late num price;
-  late int stock;
+  final String name;
+  final num price;
+  final int stock;
 
   bool get hasStock => stock > 0;
-
-  @override
-  String toString() {
-    return 'ItemSize{name: $name, price: $price, stock: $stock}';
-  }
 }
