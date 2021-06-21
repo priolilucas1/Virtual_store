a84360a47cd5f338d914371275c26e9838e7c621
priolilucas1 <priolilucas1@gmail.com>
2021-06-21 16:40:44 -0300

    Base Home Screen Initialized

---
+++ lib/src/screens/home/home_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/src/screens/home/components/section_list.dart';
+import 'package:provider/provider.dart';
+
+import 'package:loja_virtual/src/models/home_manager.dart';
+import 'package:loja_virtual/src/common/custom_drawer/custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: CustomDrawer(),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 211, 118, 130),
+                  Color.fromARGB(255, 253, 181, 168),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                snap: true,
+                floating: true,
+                backgroundColor: Colors.transparent,
+                flexibleSpace: const FlexibleSpaceBar(
+                  title: Text("Prioli's Store"),
+                  centerTitle: true,
+                ),
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.shopping_cart),
+                    color: Colors.white,
+                    onPressed: () => Navigator.of(context).pushNamed('/cart'),
+                  ),
+                ],
+              ),
+              Consumer<HomeManager>(
+                builder: (_, homeManager, __) {
+                  final List<Widget> children =
+                      homeManager.sections.map<Widget>((section) {
+                    switch (section.type) {
+                      case 'List':
+                        return SectionList(section);
+                      case 'Staggered':
+                        return Container();
+                      default:
+                        return Container();
+                    }
+                  }).toList();
+
+                  return SliverList(
+                    delegate: SliverChildListDelegate(children),
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
