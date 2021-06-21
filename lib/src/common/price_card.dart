e4e5892e606161f168c18288b645e0994212556f
priolilucas1 <priolilucas1@gmail.com>
2021-06-21 16:16:04 -0300

    Price Card Changed

--- lib/src/common/price_card.dart
+++ lib/src/common/price_card.dart
@@ -14,68 +14,89 @@ class PriceCard extends StatelessWidget {
 
     final productsPrice = cartManager.productsPrice;
 
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 16, 16, 4),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text(
-              'Resumo do pedido',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 16,
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Subtotal'),
-                Text('R\$ ${productsPrice.toStringAsFixed(2)}'),
-              ],
-            ),
-            const Divider(),
-            const SizedBox(height: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Total',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                  ),
+    if (productsPrice != 0) {
+      return Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 4),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Resumo do pedido',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
                 ),
-                Text(
-                  'R\$ 19.99',
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 16,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Subtotal'),
+                  Text('R\$ ${productsPrice.toStringAsFixed(2)}'),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('Frete'),
+                  Text('R\$ 3.00'),
+                ],
+              ),
+              const Divider(),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
+                  Text(
+                    'R\$ ${productsPrice.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor,
+                  onPrimary: Colors.white,
+                  onSurface: Theme.of(context).primaryColor.withAlpha(100),
                 ),
-              ],
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Theme.of(context).primaryColor,
-                onPrimary: Colors.white,
-                onSurface: Theme.of(context).primaryColor.withAlpha(100),
+                onPressed: onPressed,
+                child: const Text(
+                  'Continuar para Entrega',
+                  style: TextStyle(fontSize: 16),
+                ),
               ),
-              onPressed: onPressed,
-              child: const Text(
-                'Continuar para Entrega',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    } else {
+      return Container(
+        height: MediaQuery.of(context).size.height * 0.8,
+        alignment: Alignment.center,
+        child: Text(
+          'Carrinho vazio',
+          style: TextStyle(color: Colors.grey[800], fontSize: 18),
+        ),
+      );
+    }
   }
 }
