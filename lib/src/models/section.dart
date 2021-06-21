feedcd38d7a255af6e604609c937e69c04b28375
priolilucas1 <priolilucas1@gmail.com>
2021-06-21 17:13:35 -0300

    Initializer List Added

--- lib/src/models/section.dart
+++ lib/src/models/section.dart
@@ -1,23 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:loja_virtual/src/helpers/firebase_errors.dart';
 import 'package:loja_virtual/src/models/section_item.dart';
 
 class Section {
-  Section.fromDocument(DocumentSnapshot document) {
-    try {
-      if (document.exists) {
-        name = document['name'] as String;
-        type = document['type'] as String;
+  Section.fromDocument(DocumentSnapshot document)
+      : name = document['name'] as String,
+        type = document['type'] as String,
         items = (document['items'] as List)
             .map((item) => SectionItem.fromMap(item as Map<String, dynamic>))
             .toList();
-      }
-    } catch (e) {
-      getErrorString(e.toString());
-    }
-  }
 
-  late String name;
-  late String type;
-  late List<SectionItem> items;
+  final String name;
+  final String type;
+  final List<SectionItem> items;
 }
