caf997a4a6cb542cf7bf0786275c35a6a7f8820d
priolilucas1 <priolilucas1@gmail.com>
2021-06-21 16:41:31 -0300

    Section List Initialized

---
+++ lib/src/screens/home/components/section_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/src/models/section.dart';
+
+class SectionList extends StatelessWidget {
+  const SectionList(this.section);
+
+  final Section section;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
