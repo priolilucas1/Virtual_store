e033169e9f712c0c566f7934ab68bca948cedbde
priolilucas1 <priolilucas1@gmail.com>
2021-06-22 14:37:53 -0300

    Change Notifier Added

--- lib/src/models/home_manager.dart
+++ lib/src/models/home_manager.dart
@@ -1,7 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:loja_virtual/src/models/section.dart';
 
-class HomeManager {
+class HomeManager extends ChangeNotifier {
   HomeManager() {
     _loadSections();
   }
@@ -16,6 +17,7 @@ class HomeManager {
       for (final DocumentSnapshot document in snapshot.docs) {
         sections.add(Section.fromDocument(document));
       }
+      notifyListeners();
     });
   }
 }
