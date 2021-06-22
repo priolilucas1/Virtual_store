13c0cf379c442b554c12851d3626ffb2c27bf116
priolilucas1 <priolilucas1@gmail.com>
2021-06-22 14:43:44 -0300

    Section Item Done

--- lib/src/models/section_item.dart
+++ lib/src/models/section_item.dart
@@ -1,7 +1,13 @@
 class SectionItem {
-  SectionItem.fromMap(Map<String, dynamic> map) {
-    image = map['image'] as String;
-  }
+  SectionItem.fromMap(Map<String, dynamic> map)
+      : image = map['image'] as String,
+        product = map['product'] as String?;
 
-  late String image;
+  final String image;
+  final String? product;
+
+  @override
+  String toString() {
+    return 'SectionItem{image: $image, product: $product}';
+  }
 }
